8329e4b4cb42d029d850e97cc49b6d4c3eb69dfa
Aitor Urrutia <aitor.urrutia@biko2.com>
2021-04-10 12:22:14 +0200

    refactor: naming

--- gatsby-node.js
+++ gatsby-node.js
@@ -3,7 +3,7 @@ const path = require("path")
 exports.createPages = async ({ actions, graphql, reporter }) => {
   const { createPage } = actions
 
-  const blogPostTemplate = path.resolve(`src/templates/blogPost.tsx`)
+  const blogPostTemplate = path.resolve(`src/templates/BlogPost.tsx`)
 
   const result = await graphql(`
     {
